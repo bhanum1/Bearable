40c781bb9672a0cae4d989d88478ad72c29db0c5
Cairo Cristante <cairo.cristante@gmail.com>
2023-09-30 13:01:41 -0400

    fixed logo sizing

--- components/main-nav.js
+++ components/main-nav.js
@@ -9,7 +9,7 @@ export function MainNav({ items }) {
   return (
     <div className="flex gap-6 md:gap-10">
       <Link href="/" className="flex items-center space-x-2">
-        <Icons.logo className="h-6 w-6"/>
+        <Icons.logo_np className="h-6 w-6"/>
         <span className={cn(
           "font-Habibi font-extrabold xs:text-[24px] from-neutral-800 text-[30px] leading-tight tracking-tighter")}>
             {siteConfig.name}
